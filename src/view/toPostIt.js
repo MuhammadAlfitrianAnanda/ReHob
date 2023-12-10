9b9cba631d71b4822d56e028f7502c61f5ebf083
Arez762 <hasiramasenju762@gmail.com>
2023-12-10 23:37:32 +0800

    Update

--- src/view/toPostIt.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('add-icon-post').addEventListener('click', function() {
-    // Gantilah URL 'halaman-tertentu.html' dengan URL halaman yang ingin ditampilkan.
-    window.location.href = 'postIt.php';
-});
